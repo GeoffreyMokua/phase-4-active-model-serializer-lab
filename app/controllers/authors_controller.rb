7907f07f9afaf83d3a1a76aac252e7ea19f14b06
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-26 08:50:11 +0300

    completed assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,18 +3,18 @@ class AuthorsController < ApplicationController
 
   def index
     authors = Author.all 
-    render json: authors
+   
+    render json: authors, include: ['tags','posts.tags','profile','author.profile']
   end
 
   def show
     author = Author.find(params[:id])
-    render json: author
+    
+    render json: author, include: ['tags','posts.tags','profile','author.profile']
   end
 
   private
-
   def render_not_found_response
     render json: { error: "Author not found" }, status: :not_found
   end
-
 end
